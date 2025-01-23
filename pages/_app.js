7647b64cf5c5ddafd9882c010af8c7cb91a2b6a8
nikolalazarevic011 <nikola.lazaa@gmail.com>
2025-01-23 17:25:04 -0600

    test without useEffects

--- pages/_app.js
+++ pages/_app.js
@@ -16,78 +16,78 @@ function MyApp({ Component, pageProps }) {
 
   const router = useRouter();
 
-  // Redirect logged-in users away from /login and /register
-  useEffect(() => {
-    const handleRedirectIfLoggedIn = () => {
-      const user = localStorage.getItem("user");
+  // // Redirect logged-in users away from /login and /register
+  // useEffect(() => {
+  //   const handleRedirectIfLoggedIn = () => {
+  //     const user = localStorage.getItem("user");
 
-      if (
-        user &&
-        (router.pathname === "/login" || router.pathname === "/register")
-      ) {
-        router.push("/"); // Redirect to homepage if logged in
-      }
-    };
+  //     if (
+  //       user &&
+  //       (router.pathname === "/login" || router.pathname === "/register")
+  //     ) {
+  //       router.push("/"); // Redirect to homepage if logged in
+  //     }
+  //   };
 
-    handleRedirectIfLoggedIn();
+  //   handleRedirectIfLoggedIn();
 
-    // Also listen for route changes
-    router.events.on("routeChangeComplete", handleRedirectIfLoggedIn);
+  //   // Also listen for route changes
+  //   router.events.on("routeChangeComplete", handleRedirectIfLoggedIn);
 
-    // Cleanup the event listener
-    return () => {
-      router.events.off("routeChangeComplete", handleRedirectIfLoggedIn);
-    };
-  }, [router]);
+  //   // Cleanup the event listener
+  //   return () => {
+  //     router.events.off("routeChangeComplete", handleRedirectIfLoggedIn);
+  //   };
+  // }, [router]);
 
   //route protection for users without paid membership
-  useEffect(() => {
-    const handleRouteChangeStart = (url) => {
-      const user = localStorage.getItem("user");
-      const membershipId = localStorage.getItem("membershipId");
+  // useEffect(() => {
+  //   const handleRouteChangeStart = (url) => {
+  //     const user = localStorage.getItem("user");
+  //     const membershipId = localStorage.getItem("membershipId");
 
-      // Check if the route starts with /members and the user is not allowed
-      if (url.startsWith("/members") && membershipId !== "2") {
-        // Redirect to /applications/join-fma
-        router.push("/applications/join-fma");
-      }
-    };
+  //     // Check if the route starts with /members and the user is not allowed
+  //     if (url.startsWith("/members") && membershipId !== "2") {
+  //       // Redirect to /applications/join-fma
+  //       router.push("/applications/join-fma");
+  //     }
+  //   };
 
-    // Add event listener for route changes
-    router.events.on("routeChangeStart", handleRouteChangeStart);
+  //   // Add event listener for route changes
+  //   router.events.on("routeChangeStart", handleRouteChangeStart);
 
-    // Clean up the event listener on component unmount
-    return () => {
-      router.events.off("routeChangeStart", handleRouteChangeStart);
-    };
-  }, [router]);
+  //   // Clean up the event listener on component unmount
+  //   return () => {
+  //     router.events.off("routeChangeStart", handleRouteChangeStart);
+  //   };
+  // }, [router]);
 
-  // Fetch menu data on route change
-  useEffect(() => {
-    const handleRouteChangeComplete = async () => {
-      try {
-        const { props } = await getPageStaticProps({ params: { slug: [] } });
-        setMenuData({
-          mainMenuItems: props.mainMenuItems,
-          footerMenuItems: props.footerMenuItems,
-        });
-      } catch (error) {
-        console.error("Error fetching menu data:", error);
-      }
-    };
+  // // Fetch menu data on route change
+  // useEffect(() => {
+  //   const handleRouteChangeComplete = async () => {
+  //     try {
+  //       const { props } = await getPageStaticProps({ params: { slug: [] } });
+  //       setMenuData({
+  //         mainMenuItems: props.mainMenuItems,
+  //         footerMenuItems: props.footerMenuItems,
+  //       });
+  //     } catch (error) {
+  //       console.error("Error fetching menu data:", error);
+  //     }
+  //   };
 
-    // Fetch on route change
-    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+  //   // Fetch on route change
+  //   router.events.on("routeChangeComplete", handleRouteChangeComplete);
 
-    // Fetch immediately if the data is empty
-    if (!menuData.mainMenuItems.length || !menuData.footerMenuItems.length) {
-      handleRouteChangeComplete();
-    }
+  //   // Fetch immediately if the data is empty
+  //   if (!menuData.mainMenuItems.length || !menuData.footerMenuItems.length) {
+  //     handleRouteChangeComplete();
+  //   }
 
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChangeComplete);
-    };
-  }, [router, menuData.mainMenuItems.length, menuData.footerMenuItems.length]);
+  //   return () => {
+  //     router.events.off("routeChangeComplete", handleRouteChangeComplete);
+  //   };
+  // }, [router, menuData.mainMenuItems.length, menuData.footerMenuItems.length]);
 
   return (
     <Layout
