2720570c11ca809093017608cf7675830c8cbdfe
nikolalazarevic011 <nikola.lazaa@gmail.com>
2025-01-23 16:27:52 -0600

    almost worked, dynamic routes didn't show, trying making benefits page

--- next.config.js
+++ next.config.js
@@ -1,28 +1,12 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     domains: [process.env.WP_IMAGES_URL],
-//     unoptimized: true, // Disable the default image optimization to support static exports
-//   },
-//   output: "export", // Set the build output for static export
-//   basePath: "/next", //only for prod
-// };
-
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [process.env.WP_IMAGES_URL],
-    unoptimized: process.env.NODE_ENV === "production", // Disable optimization in production for static export
+  },
+  experimental: {
+    scrollRestoration: true,
   },
 };
-
-// Conditionally apply settings for production
-if (process.env.NODE_ENV === "production") {
-  nextConfig.output = "export"; // Enable static export only in production
-}
 
 module.exports = nextConfig;
