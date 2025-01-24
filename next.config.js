f942fd0153a30c2116ea0aab1797176d6d574aac
nikolalazarevic011 <nikola.lazaa@gmail.com>
2025-01-24 16:50:35 -0600

    build 6 test

--- next.config.js
+++ next.config.js
@@ -2,14 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // Disable image optimization for static export
+    domains: [process.env.WP_IMAGES_URL],
   },
   experimental: {
     scrollRestoration: true,
   },
-  basePath: '/frontend', // Base path for serving files under /frontend
-  trailingSlash: true,   // Ensures all routes include a trailing slash
-  // output: 'export',      // Use static export
 };
 
 module.exports = nextConfig;
